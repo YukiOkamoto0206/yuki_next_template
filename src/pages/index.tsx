1b1d8d3e2d8cdb3009c22da40a1976fc89d8e37d
YukiOkamoto0206 <okamotoyu@crunchtimer.jp>
2022-03-08 20:35:37 -0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-9xl font-extrabold text-green-600 text-center">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Home
